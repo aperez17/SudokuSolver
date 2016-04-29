837f224f8b81465f42a4dd3abea3914a8324fdb5
Alex Perez <alex.raf.bran-perez@hp.com>
2016-04-29 13:56:59 -0400

    some more refactoring

--- src/main/scala/SudokuSolver/bruteForce.scala
+++ src/main/scala/SudokuSolver/bruteForce.scala
@@ -3,19 +3,21 @@ object BruteForce extends SudokuSolver{
   
   def solve(puzzle: Puzzle): Seq[Seq[Option[Int]]] = {
     val asSeq = Puzzle.boardToSeq(puzzle.board).map(_.map(_.getOrElse(0)))
-    val n = asSeq.size
-    val s = Math.sqrt(n).toInt
-    val solution = (solveBoard(asSeq, n = n, s = s).getOrElse(IndexedSeq.empty[IndexedSeq[Int]])).toSeq
+    val size = asSeq.size
+    val s = Math.sqrt(size).toInt
+    val solution = (solveBoard(asSeq, size = size, square = s).getOrElse(IndexedSeq.empty[IndexedSeq[Int]])).toSeq
     solution.map(_.toSeq.map(Some(_)))
   }
   
-  def solveBoard(board: Board, cell: Int = 0, n: Int, s: Int): Option[Board] = (cell%n, cell/n) match {
-    case (r, `n`) => Some(board)
-    case (r, c) if board(r)(c) > 0 => solveBoard(board, cell + 1, n, s)
-    case (r, c) =>
-      def cells(i: Int) = Seq(board(r)(i), board(i)(c), board(s*(r/s) + i/s)(s*(c/s) + i%s))
+  def solveBoard(board: Board, cell: Int = 0, size: Int, square: Int): Option[Board] = (cell%size, cell/size) match {
+    case (rIndex, `size`) => Some(board)
+    case (rIndex, c) if board(rIndex)(c) > 0 => solveBoard(board, cell + 1, size, square)
+    case (rIndex, c) =>
+      def cells(i: Int) = Seq(board(rIndex)(i), board(i)(c), board(square*(rIndex/square) + i/square)(square*(c/square) + i%square))
 
-      def guess(x: Int) = solveBoard(board.updated(r, board(r).updated(c, x)), cell + 1, n, s)
-        1 to n diff (board.indices flatMap cells) collectFirst Function.unlift(guess)
+      def tryNumber(x: Int): Option[Board] = solveBoard(board.updated(rIndex, board(rIndex).updated(c, x)), cell + 1, size, square)
+      val numbers = 1 to size
+      val openNumbers = numbers.diff(board.indices.flatMap(cells)) 
+      openNumbers.collectFirst(Function.unlift(tryNumber))
   }
 }
